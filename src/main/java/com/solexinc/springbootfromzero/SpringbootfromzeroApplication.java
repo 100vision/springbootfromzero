03a1ace685791b29e4db992dae559ee458c76844
tixiang_lin <tixiang_lin@solex.com>
2021-01-08 14:46:14 +0800

    init commit

---
+++ src/main/java/com/solexinc/springbootfromzero/SpringbootfromzeroApplication.java
@@ -0,0 +1,13 @@
+package com.solexinc.springbootfromzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfromzeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootfromzeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solexinc/springbootfromzero/SpringbootfromzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solexinc.springbootfromzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootfromzeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
